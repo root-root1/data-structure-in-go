469c99e1969258a4393ea76b66f560b6500615cb
root-root1 <ar6010461@gmail.com>
2021-09-11 10:40:52 +0530

    squeue and queue add

--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
 
-import (
-	"fmt"
-
-	set "github.com/root_root1/conTut/data/test/set"
-)
-
 func main() {
-	// var list linkedlist.LinkedList
-	// list = linkedlist.LinkedList{}
-	var setData *set.Set
-	setData = &set.Set{}
-	setData.New()
-	setData.AddElement(1)
-	setData.AddElement(2)
-	fmt.Println("Initial Set ", setData)
-	fmt.Println(setData.ContainsElement(1))
-	var anotherSet *set.Set
-	anotherSet = &set.Set{}
-	anotherSet.New()
-	anotherSet.AddElement(2)
-	anotherSet.AddElement(4)
-	anotherSet.AddElement(5)
-	fmt.Println(setData.Intersect(anotherSet))
-	fmt.Println(setData.Union(anotherSet))
+	// var queue *queue.Queue = &queue.Queue{}
+	// queue.New()
+	// fmt.Println(queue)
+	// for i := 0; i < 10; i += 1 {
+	// 	go queue.Passenger(queue)
+	// }
 }
